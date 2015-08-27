a013e0bc8d13caf80e1b05b9fa97e64a34d8308a
dzonz <dzonz@horisen.com>
2015-08-27 16:29:33 +0200

    resolved gallery params & test

--- README.md
+++ README.md
@@ -1,1 +1,280 @@
 # proto-gallery-js
+
+## Table of Contents
+
+- [Usage](#usage)
+- [Options](#options)
+    - [Actions](#actions)
+    - [onAction](#onaction)
+    - [onInit](#oninit)
+    - [onOpen](#onopen)
+    - [onOpened](#onopened)
+	- [onClose](#onclose)
+	- [onClosed](#onclosed)
+    - [onResize](#onresize)
+- [Methods](#methods)
+    - [open](#open)
+    - [close](#close)
+    - [settings](#settings)
+- [Advanced usage](#advanced-usage)
+    - [Dialog](#dialog)
+    - [Controller](#controller)
+
+## Usage
+
+```html
+<link rel="stylesheet" type="text/css" href="proto-dialog.css"/>
+
+<!-- ... -->
+
+<script type="text/javascript" src="jquery.proto-dialog.js"></script>
+
+<!-- ... -->
+
+<div id="proto-dialog">
+    <div id="proto-dialog-header"></div>
+    <div id="proto-dialog-content"></div>
+    <div id="proto-dialog-footer"></div>
+<!-- ... -->
+</div>
+```
+
+Add the UI.Layout module as a dependency to your application module:
+
+```javascript
+$('#proto-dialog').protoDialog({ ... });
+```
+
+## Options
+
+```javascript
+var protoDialogOptions = {
+    actions: {
+    },
+    animation: {
+        'open': false,
+        'close': false
+    },
+    onInit: function($dialog) {
+    },
+    onOpen: function($dialog) {
+    },
+    onOpened: function($dialog) {
+    },
+    onClose: function($dialog) {
+    },
+    onClosed: function($dialog) {
+    },
+    onResize: function($dialog) {
+    },
+    onAction: function(_action, $dialog) {
+    }
+};
+```
+
+### actions
+
+Type: `Object`
+Default: `{}`
+
+Define buttons that you want on action bar
+
+```javascript
+var actions = {
+    'actionOne': {
+        'label': 'button label',
+        'class': 'html classes',
+        'on': {
+            'click': function(event, $dialog) {
+                $(this).html('custom - custom on click event');
+            },
+            'mouseover': function(event, $dialog) {
+                $(this).html('custom - custom on mouse over event');
+            }
+        }
+    },
+    'actionTwo': {
+        'label': 'button label',
+        'class': 'html classes'
+    },
+    'actionTree': {
+        'label': 'button label',
+        'class': 'html classes'
+    }
+};
+```
+
+### onAction
+
+Type: `Function`
+Default: `function ($_action, _event, $dialog) {}`
+
+This method is called each time when an action button is clicked. If you want to have full control on action button events use the `on` key in `actions`.
+
+```javascript
+var onAction = function (action, _event, $dialog) {
+    switch(_action) {
+        case 'actionTwo':
+            /* Some specific action for button actionTwo */
+            break;
+        default:
+            $(this).html(_action + ' - auto click event');
+            break;
+    }
+};
+```
+
+### onInit
+
+Type: `Function`
+Default: `function ($dialog) {}`
+
+This method is called when the dialog is initialized for the first time, and it is called one time only from the widget.
+
+```javascript
+var onInit: function ($dialog) {
+    var protoDialogContent = $dialog.find('.proto-dialog-content');
+    var dialogFooter = $dialog.find('#proto-dialog-footer');
+    var dialogHeader = $(this).find('#proto-dialog-header');
+};
+```
+
+### onOpen
+
+Type: `Function`
+Default: `function ($dialog) {}`
+
+This method is called each time before the dialog is to be opened.
+
+### onOpened
+
+Type: `Function`
+Default: `function ($dialog) {}`
+
+This method is called each time after the dialog has been opened.
+
+### onClose
+
+Type: `Function`
+Default: `function ($dialog) {}`
+
+This method is called each time before the dialog is to be closed.
+
+### onClosed
+
+Type: `Function`
+Default: `function ($dialog) {}`
+
+This method is called each time after the dialog has been closed.
+
+### onResize
+
+Type: `Function`
+Default: `function ($dialog) {}`
+
+This method is called each time when the screen size changes.
+
+## Methods
+
+Take a look at the list of methods to control the dialog.
+
+### open
+
+This method is called to open a dialog manually.
+
+```javascript
+$('#proto-dialog').protoDialog('open', {
+    ['title': ... ]
+    [, 'content': ... ]
+});
+```
+
+### close
+
+This method is called to close a dialog manually.
+
+```javascript
+$('#proto-dialog').protoDialog('close');
+```
+
+### settings
+
+This method is called when you want to override settings.
+
+```javascript
+$('#proto-dialog').protoDialog('settings', { ... });
+```
+
+## Advanced usage
+
+The advanced usage is about taking full control of the dialog.
+
+### Dialog
+
+Take a look at the dialog HTML structure in order to have a better view, and push your ideas further.
+When ever you have a `$dialog` variable in a `on***` function, this will be your `$('.proto-dialog-overlay')` object.
+The `<!-- Dialog content -->` is replaced by your target object that the widget is applied to.
+
+```html
+<div class="proto-dialog-overlay">
+    <div class="proto-dialog-window">
+        <div class="proto-dialog-inner">
+            <button type="button" class="proto-dialog-close proto-dialog-button"></button>
+            <div class="proto-dialog-title">
+                <h3><!-- Title --></h3>
+            </div>
+            <div class="proto-dialog-content">
+                <!-- Dialog content -->
+            </div>
+            <div class="proto-dialog-actions">
+                <!-- Action buttons -->
+            </div>
+        </div>
+    </div>
+</div>
+```
+
+### Controller
+
+```javascript
+var controllerInterface = {
+    /**
+     * Closes the dialog
+     */
+    'close': function () {},
+    /**
+     * Openes the dialog
+     */
+    'open': function () {},
+    /**
+     * Is dialog opened
+     */
+    'isOpened': function () {},
+    /**
+     * Sets the title
+     */
+    'setTitle': function () {},
+    /**
+     * Sets the content
+     */
+    'setContent': function () {},
+    /**
+     * Defines new settings
+     */
+    'setSettings': function () {},
+    /**
+     * Get current settings
+     */
+    'getSettings': function () {},
+};
+
+/* Get dialog controller instance */
+var _data = $('#proto-dialog').data('protoDialog');
+
+// If the widget is initialized
+if (_data) {
+    var _controller = _data['controller'];
+    var _settings = _data['settings'];
+    var $target = _data['target']; // === $('#proto-dialog')
+}
+```
--- jquery.proto-gallery.js
+++ jquery.proto-gallery.js
@@ -3,30 +3,15 @@
  * @author dzonz
  */
 (function($) {
+// Generate a single only dialog
+    var $canvas =  $("#proto-gallery-canvas");
+    var $loader = $('#proto-gallery-loader');
+    var $slider = $('#proto-gallery-slider');
+    var $info = $('#proto-gallery-slider');
+    var $slideTitle = $('#proto-gallery-slider');
+    var $slideDescription = $('#proto-gallery-slider');
 
-    var _getSelectorValue = function ($source, _selectorSchema) {
-        var _schema = _selectorSchema.split('#');
-        if (!_schema[1]) {
-            return $source.find(_schema[0]).html();
-        } else {
-            return $source.find(_schema[0]).attr(_schema[1]);
-        }
-    };
-
-    var _protoGalleryControllerClass = function($target, _settings) {
-        var _instance = _settings;
-        var _slides = [];
-        var _currentSlide = 0;
-        var $currentSlide = null;
-        var _slideCounter = 0;
-        
-        var $canvas =  $("#proto-gallery-canvas");
-        var $loader = $('#proto-gallery-loader');
-        var $slider = $('#proto-gallery-slider');
-        var $info = $('#proto-gallery-slider');
-        var $slideTitle = $('#proto-gallery-slider');
-        var $slideDescription = $('#proto-gallery-slider');
-        
+    $(document).ready(function () {
         if ($canvas.length == 0) {
             $canvas =  $('<div class="proto-gallery-content" id="proto-gallery-content"></div>');
             $loader =  $('<div class="proto-gallery-loader" id="proto-gallery-loader"></div>');
@@ -34,47 +19,175 @@
             $info =  $('<div class="proto-gallery-info" id="proto-gallery-info"></div>');
             $slideTitle =  $('<h3 class="proto-gallery-slide-title" id="proto-gallery-slide-title"></h3>');
             $slideDescription =  $('<div class="proto-gallery-slide-description" id="proto-gallery-slide-description"></div>');
-            
+
             $info.append($slideTitle).append($slideDescription);
-            
+
             $canvas.append($loader).append($slider).append($info);
-            
+
             $('body').append($canvas);
         }
+
+        $($canvas).protoDialog();
+    });
+     
+// selector finder
+    var _getSelectorValue = function ($source, _selectorSchema) {
+        var _schema = _selectorSchema.split('#');
+        if (!_schema[1]) {
+            return $source.find(_schema[0]).html();
+        } else if (_schema[0].length == 0 && _schema[1]) {
+            return $source.attr(_schema[1]);
+        } else if (_schema[0] && _schema[1]) {
+            return $source.find(_schema[0]).attr(_schema[1]);
+        }
         
-        this.settings = function(newSettings) {
+        return false;
+    };
+
+// gallery controller
+    var _protoGalleryControllerClass = function($target, _settings) {
+        var _instance = null;
+        var _slides = [];
+        var _currentSlide = 0;
+        var $currentSlide = null;
+        var _slideCounter = 0;
+        
+        this.setSettings = function(newSettings) {
+            var oldSettings = _instance;
+            
             _instance = newSettings;
+            
+            if (_instance['source']) {
+                switch (_instance['source']) {
+                    case 'selector':
+                        this.setSlides($target.find(_instance.selector.slide));
+                        break;
+                    case 'query':
+                        this.setSlides($(_instance.query));
+                        break;
+                    case 'collection':
+                        this.setCollection(_instance.collection);
+                        break;
+                }
+            } 
+        };
+
+        this.getSettings = function () {
+            return _instance;
         };
 
         this.open = function() {
-            $canvas.protoDialog('open');
+            
+            $canvas.protoDialog('open', {
+                actions: $.extend({
+                    'prev': {
+                        'label': _instance.prevButton,
+                        'class': 'btn btn-default btn-nav btn-nav-previous'
+                    },
+                    'next': {
+                        'label': _instance.nextButton,
+                        'class': 'btn btn-default btn-nav btn-nav-next'
+                    }
+                }, _instance.actions),
+                onInit: function($dialog) {                    
+                    _instance.onInit.call($target, $dialog, _this);
+                },
+                onOpen: function($dialog) {
+                    _this.noSlide();
+                    $dialog.addClass('proto-gallery-overlay');
+                    _instance.onOpen.call($target, $dialog, _this);
+                },
+                onOpened: function($dialog) {
+                    _instance.onOpened.call($target, $dialog, _this);
+                },
+                onClose: function($dialog) {
+                    _this.noSlide();
+                    _instance.onClose.call($target, $dialog, _this);
+                },
+                onClosed: function($dialog) {
+                    _instance.onClosed.call($target, $dialog, _this);
+                },
+                onResize: function($dialog) {
+                    _resizeSlide();
+                    _instance.onResize.call($target, $dialog, _this);
+                },
+                onAction: function(_action, $dialog, event) {
+                    switch (_action) {
+                        case 'prev':
+                            _this.previousSlide();
+                            break;
+                        case 'next':
+                            _this.nextSlide();
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            });
 //            _instance.onOpen.call($target, $canvas);
 //            $canvas.show();
         };
 
         this.close = function() {
+            this.noSlide();
+            
             $canvas.protoDialog('close');
-//            _instance.onClose.call($target, $canvas);
-//            $canvas.hide();
-        };        
-
-        this.addSlide = function($slide) {
+        };
+        
+        this.setCollection = function (_collection) {
+            _slides = [];
+            _currentSlide = 0;
+            $currentSlide = null;
+            _slideCounter = 0;
+            
+            this.addCollection(_collection);
+        };
+        
+        this.addCollection = function (_collection) {
+            for (var _itemIndex in _collection) {
+                var _itemData = _collection[_itemIndex];
+                this.addCollectionItem(_itemData);
+            }
+        };
+        
+        this.addCollectionItem = function (_item) {
             _slideCounter++;
             
-            _slides.push({
-                'element': $slide,
-                'link': _getSelectorValue($slide, _instance.slideLinkSelector),
-                'thumbnail': _getSelectorValue($slide, _instance.slideThumbnailSelector),
-                'photo': _getSelectorValue($slide, _instance.slidePhotoSelector),
-                'title': _getSelectorValue($slide, _instance.slideTitleSelector),
-                'description': _getSelectorValue($slide, _instance.slideDescriptionSelector)
+            _slides.push(_item);
+        };
+        
+        this.setSlides = function ($slides) {
+            _slides = [];
+            _currentSlide = 0;
+            $currentSlide = null;
+            _slideCounter = 0;
+        
+            this.attachSlides($slides);
+        };
+        
+        this.attachSlides = function ($slides) {
+            $slides.each(function() {
+                $(this).off('click');
+                _this.attachSlide($(this));
             });
-
+        };
+        
+        this.attachSlide = function($slide) {
+            this.addCollectionItem({
+                'element': $slide,
+                'link': _getSelectorValue($slide, _instance.selector.link),
+                'thumbnail': _getSelectorValue($slide, _instance.selector.thumbnail),
+                'photo': _getSelectorValue($slide, _instance.selector.photo),
+                'title': _getSelectorValue($slide, _instance.selector.title),
+                'description': _getSelectorValue($slide, _instance.selector.description)
+            });
+            
             $slide.data('slideIndex', _slideCounter);
 
             $slide.click(function(e) {
                 e.preventDefault();
                 e.stopPropagation();
+                
                 // raise gallery fromthis image
                 _this.open();
                 _this.showSlide($(this).data('slideIndex'));
@@ -83,21 +196,20 @@
             });
         };
 
-        this.removeSlide = function(_index) {
+        this.removeCollectionItem = function(_index) {
             _slides.splice(_index, 1);
+        };
+
+        this.getSlideCount = function() {
+            return _slides.length;
         };
 
         this.getSlide = function(_index) {
             return _slides[_index];
         };
 
-        this.setSlide = function(_index, $thumbnail, $full, $width, $height) {
-            _slides[_index] = {
-                'thumbnail': $thumbnail,
-                'full': $full,
-                'width': $width,
-                'height': $height
-            };
+        this.noSlide = function () {
+            $slider.find('> *').remove();
         };
 
         this.firstSlide = function() {
@@ -121,12 +233,16 @@
         };
 
         this.showSlide = function(_index) {
+            var _oldSlide = _currentSlide;
+            
             _currentSlide = _index;
             if (_currentSlide < 0) {
                 _currentSlide = _slides.length - 1;
             }
             _currentSlide = _currentSlide % _slides.length;
+            
             if (false !== _instance.onChangeSlide.call($target, _currentSlide, $canvas)) {
+                
                 var _slide = _slides[_currentSlide];
                 var $slide = $('<img src="' + _slide['photo'] + '"/>');
                 $slide.css({
@@ -134,10 +250,12 @@
                     'z-index': '1'
                 });
                 
+                $loader.hide();
                 $loader.show().fadeIn(0);
-                
+//                alert(_index);
                 var _showSlide = function () {
-                    $loader.finish().fadeOut(300, function () {
+                    
+                    $loader.finish().fadeOut(500, function () {
                         $(this).hide();
                     });
                     $slide.fadeOut(0);
@@ -159,17 +277,14 @@
                     var $previous = $slider.find('> *');
                     
                     if ($previous.length > 0) {
-//                        $previous.css('z-index', '2').fadeOut(500, function() {
-//                            $(this).remove();
-//                            _showSlide();
-//                        });
                         $previous.css('z-index', '2').animate({
                             'left': '-100%'
                         }, function() {
                             $(this).remove();
-                            _showSlide();
                         });
+                        _showSlide();
                     } else {
+//                        alert(_slide);
                         _showSlide(_slide);
                     }
                 });
@@ -211,69 +326,24 @@
             }
         };
 
-        this.settings(_settings);
-
-        var $slides = $target.find(_instance.slideSelector);
+        this.setSettings(_settings);
         
-        $slides.each(function() {
-            _this.addSlide($(this));
-        });
-
-        $canvas.protoDialog({
-            actions: $.extend({
-                'prev': {
-                    'label': 'Previous',
-                    'class': 'btn btn-default btn-nav btn-nav-previous'
-                },
-                'next': {
-                    'label': 'Next',
-                    'class': 'btn btn-default btn-nav btn-nav-next'
-                }
-            }, _instance.actions),
-            onInit: function($dialog) {
-                $dialog.addClass('proto-gallery-overlay');
-                _instance.onInit.call($target, $dialog, _this)
-            },
-            onOpen: function($dialog) {
-                _instance.onOpen.call($target, $dialog, _this)
-            },
-            onOpened: function($dialog) {
-                _instance.onOpened.call($target, $dialog, _this)
-            },
-            onClose: function($dialog) {
-                _instance.onClose.call($target, $dialog, _this)
-            },
-            onClosed: function($dialog) {
-                _instance.onClosed.call($target, $dialog, _this)
-            },
-            onResize: function($dialog) {
-                _resizeSlide();
-                _instance.onResize.call($target, $dialog, _this)
-            },
-            onAction: function(_action, $dialog, event) {
-                switch (_action) {
-                    case 'prev':
-                        _this.previousSlide();
-                        break;
-                    case 'next':
-                        _this.nextSlide();
-                        break;
-                    default:
-                        break;
-                }
-            }
-        });
     };
 
     var methods = {
         init: function(options) {
             var _settings = $.extend({
-                'slideSelector' : 'figure',
-                'slideThumbnailSelector' : 'img#src',
-                'slidePhotoSelector' : 'a#data-photo',
-                'slideTitleSelector' : 'img#alt',
-                'slideDescriptionSelector' : 'div',
-                'slideLinkSelector' : 'a#href',
+                'source' : 'selector', // selector, query, collection
+                'selector': {
+                    'slide': 'a',
+                    'thumbnail': 'img#src',
+                    'photo': '#data-photo',
+                    'title': 'img#alt',
+                    'description': 'div',
+                    'link': '#href'
+                },
+                'collection': null,
+                'query': null,
                 prevButton: '<',
                 nextButton: '>',
                 closeButton: 'X',
@@ -323,7 +393,7 @@
                 $(this).data('protoGallery', _data);
             });
         },
-        open: function( ) {
+        open: function(_settings) {
             return this.each(function() {
                 var $this = $(this);
                 var _data = $this.data('protoGallery');
@@ -331,8 +401,20 @@
                 // If the plugin hasn't been initialized yet
                 if (_data) {
                     // search for elements
+                    
                     var _controller = _data['controller'];
+                    
+                    if (_settings) {
+                        _data['settings'] = $.extend(true, _controller.getSettings(), _settings);
+                        
+                        _controller.setSettings(_data['settings']);
+                        
+                        $this.data('protoGallery', _data);
+                    }
+                    
                     _controller.open();
+                    
+                    _controller.firstSlide();
                 }
             });
         },
@@ -358,13 +440,15 @@
                 if (_data) {
                     // search for elements
                     var _controller = _data['controller'];
-                    _data['settings'] = _settings;
+                    
+                    _data['settings'] = $.extend(true, _controller.getSettings(), _settings);
+                    
                     $this.data('protoGallery', _data);
-                    _controller.settings(_settings);
+                    
+                    _controller.setSettings(_data['settings']);
                 }
             });
         }
-
     };
 
     $.fn.protoGallery = function(method) {
